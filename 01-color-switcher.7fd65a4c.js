309f17a5c063b39aa1fa69ea4f453dc026710af0
sherwinmanosa <145818525+sherwinmanosa@users.noreply.github.com>
2024-04-23 13:13:46 +0000

    Deploying to gh-pages from @ sherwinmanosa/goit-js-hw-09@6d5c0cb4ee6d6d6d7eac62ddef403a57afa605df 🚀

---
+++ 01-color-switcher.7fd65a4c.js
@@ -0,0 +1,2 @@
+!function(){const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");let n=null;d.disabled=!0,e.addEventListener("click",(function(){n=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,d.disabled=!1,e.disabled=!0}),1e3)})),d.addEventListener("click",(function(){clearInterval(n),d.disabled=!0,e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.7fd65a4c.js.map
